e4ead5dd5aa81590e2a66c2b1ff98cda933bb7b2
Hendryboyz <hendry.js1247@gmail.com>
2025-02-26 09:36:10 +0800

    fix: postgres rds deny public access

--- default-vpc.tf
+++ default-vpc.tf
@@ -19,3 +19,40 @@ data "aws_subnets" "default" {
     values = [aws_default_vpc.default.id]
   }
 }
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_default_vpc.default.id
+
+  ingress = [
+    {
+      protocol         = -1
+      description      = "Default inbound rule"
+      from_port        = 0
+      to_port          = 0
+      self             = true
+      security_groups  = []
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+    },
+    {
+      protocol         = "tcp"
+      description      = "Free tier PostgreSQL inbound rule"
+      from_port        = 5432
+      to_port          = 5432
+      self             = false
+      security_groups  = []
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+    }
+  ]
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    description = "Default outbound rule"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
