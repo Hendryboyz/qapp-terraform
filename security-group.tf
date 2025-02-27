96ea55ba3be6a4c8d0b31271fec7871dc16c520e
Hendryboyz <hendry.js1247@gmail.com>
2025-02-27 09:30:34 +0800

    fix: target group health check ecs service failed

--- security-group.tf
+++ security-group.tf
@@ -7,25 +7,12 @@ resource "aws_security_group" "ecs_container_instance" {
 
   ingress = [
     {
-      description      = "Allow client ingress traffic from ALB on HTTP only"
+      description      = "Allow client ingress traffic from ALB on 3000 only"
       from_port        = 3000
       to_port          = 3000
       protocol         = "tcp"
-      security_groups  = [aws_security_group.alb.id]
+      security_groups  = [ "${aws_security_group.alb.id}" ]
       self             = true
-      security_groups  = []
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      cidr_blocks      = []
-    },
-    {
-      description      = "Allow backend ingress traffic from ALB on HTTP only"
-      from_port        = 4200
-      to_port          = 4200
-      protocol         = "tcp"
-      security_groups  = [aws_security_group.alb.id]
-      self             = true
-      security_groups  = []
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       cidr_blocks      = []
@@ -44,6 +31,8 @@ resource "aws_security_group" "ecs_container_instance" {
     Name        = "${var.app_name}-${var.environment}-ECS_Task-SecurityGroup"
     Environment = var.environment
   }
+
+  depends_on = [ aws_security_group.alb ]
 }
 
 ## Security Group for ALB
@@ -51,6 +40,33 @@ resource "aws_security_group" "alb" {
   name        = "${var.app_name}-${var.environment}-alb-security_group"
   description = "Security group for ALB"
   vpc_id      = aws_default_vpc.default.id
+
+  ingress = [
+    {
+      description      = "Allow traffic to ALB on HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      security_groups  = []
+      self             = true
+      security_groups  = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+      description      = "Allow traffic to ALB on HTTPs"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      security_groups  = []
+      self             = true
+      security_groups  = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
 
   egress {
     description = "Allow all egress traffic"
